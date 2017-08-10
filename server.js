2de621bf0985efe5952c0a58f08e5d887cdd92c2
gameads <adarsh.srivatsa98@gmail.com>
2017-08-10 13:23:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/file-1', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'file-1.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
