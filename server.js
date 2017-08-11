315a0776fa5e83d951053ee0be54e37a7606b7b3
gameads <adarsh.srivatsa98@gmail.com>
2017-08-11 21:06:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var counter=0;
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/counter', function (req, res) {
+ counter+=1;
+ res.send(counter.toString());
+});
+
 
 app.get('/file-1', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'file-1.html'));
