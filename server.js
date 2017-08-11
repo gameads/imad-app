58d079b7e25910f19810e5753edb0a69cbced2fb
gameads <adarsh.srivatsa98@gmail.com>
2017-08-11 21:41:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 app.get('/counter', function (req, res) {
  counter+=1;
@@ -20,9 +19,9 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
+//app.get('/', function (req, res) {
+ //res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+//});
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
